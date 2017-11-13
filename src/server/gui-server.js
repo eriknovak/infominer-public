016ab7752ccbc92101dc8b6da20425175bf0e738
ErikNovak <novak.erik@gmail.com>
2017-11-13 05:42:35 +0100

    Change readme; save new module dependancies; use postgresql for metadata storing

--- src/server/gui-server.js
+++ src/server/gui-server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// internal modules
+const pg = require('../lib/postgresQL')(require('../config/pgconfig')); // postgres connection
+
 // parameters used on the express app
 const PORT = process.env.PORT || process.env.npm_package_config_portGui || 3000;
 
@@ -14,11 +17,12 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
     extended: true
 }));
+
 // set where are public files
 app.use(express.static(__dirname + '/public'));
 
 // upload api routes
-require('./routes/api')(app);
+require('./routes/api')(app, pg);
 
 // handle ember web application
 // IMPORTANT: must be after all routes
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,39 +1,85 @@
 // external modules
-const multer = require('multer');
-const request = require('request-promise-native');
+const multer = require('multer');   // accepting files from client
+const stream = require('stream');   // create stream from file buffer
+const fs = require('fs');
+
+// internal modules
+const fileManager = require('../../lib/fileManager');
+
+// configurations
+const static = require('../../config/static');
+
 // parameter values
 let upload = multer();
 
 /**
  * Adds api routes to express  app.
  * @param {Object} app - Express app.
+ * @param {Object} pg - Postgres wrapper.
  */
-module.exports = function (app) {
-
-    // data server domain
-    const dataDomain = `http://localhost:${process.env.npm_package_config_portData}`;
+module.exports = function (app, pg) {
 
     // TODO: ensure handling large datasets
     app.post('/api/dataset/new', upload.single('file'), (req, res) => {
+        // TODO: log calling this route
+
         // get dataset info
         let { dataset, fields } = req.body;
         let file = req.file;
-        // prepare options for posting to data server
-        let options = {
-            method: 'POST',
-            uri: `${dataDomain}${req.originalUrl}`,
-            body: {
-                dataset: JSON.parse(dataset),   // dataset info
-                fields: JSON.parse(fields),     // fields for the store
-                file: file                      // file object \w content
-            },
-            json: true // Automatically stringifies the body to JSON
-        };
-        // make the request
-        request(options)
-            .then(body => { console.log('Body', body); })
-            .catch(err => { console.log('Err', err); })
-            .then(() => res.end());
 
-    });
+        // parse the JSON values from the body
+        dataset = JSON.parse(dataset);
+        fields = JSON.parse(fields);
+
+        // TODO: get username of creator
+        const creator = 'user'; // temporary placeholder
+
+        // get number of datasets the users already has
+        pg.select({ creator }, 'datasets', (err, results) => {
+            if (err) {
+                // TODO: handle error
+                console.log(err); return;
+            }
+            // number of datasets is the name of the new dataset folder
+            const numOfDatasets = results.length;
+
+            // set pg dataset values
+            const label = dataset.label || 'root';                        // the user defined dataset label
+            const dir = `${static.dataPath}/${creator}/${numOfDatasets}`; // dataset directory
+            const sourcefile = file.originalname;                         // file original name
+
+            // create dataset directory
+            fileManager.createDirectoryPath(dir);
+
+            // create write stream in dataset folder
+            let filePath = `${dir}/${file.originalname}`;
+            let writeStream = fs.createWriteStream(filePath);
+
+            // save file in corresponding dataset folder
+            let bufferStream = new stream.PassThrough();
+            bufferStream.end(file.buffer);
+            bufferStream.pipe(writeStream)
+                .on('finish', () => {
+                    // TODO: log completion
+                    console.log('complete');
+                    // save the metadata to postgres
+                    pg.insert({ creator, label, dir, sourcefile }, 'datasets', (err, results) => {
+                        if (err) {
+                            // TODO: handle error
+                            console.log(err); return;
+                        }
+                        // TODO: log postgres saving
+
+                        // TODO: create a new child process
+
+                        // end connection
+                        return res.end();
+                    });
+
+                }); // bufferStream.on('finish')
+
+        }); // pg.select({ creator })
+    }); // POST /api/dataset/new
+
+
 };
