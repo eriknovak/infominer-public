cfd18a88c925992c7b1afe55536bde83579af270
ErikNovak <novak.erik@gmail.com>
2017-11-29 22:05:18 +0100

    Table style and selection function

--- src/server/ember-source/app/components/document-table-row/component.js
+++ src/server/ember-source/app/components/document-table-row/component.js
@@ -1,22 +1,46 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    // component class
+    classNameBindings: ['selected'],
+    selected: false,
+    // component tag
     tagName: 'tr',
+
+    ///////////////////////////////////////////////////////
+    // Component Life Cycle
+    ///////////////////////////////////////////////////////
 
     didReceiveAttrs() {
         this._super(...arguments);
+        // set component id
+        this.set("elementId", `document-${this.get('index')}`);
+
         // get document valueObject and fields
         const valueObject = this.get('valueObject');
         const fields = this.get('fields');
 
-        let values = [ ];
+        // save document values
+        let docValues = [ ];
         // get values in the fields order
         for (let field of fields) {
             // console.log(field);
-            values.push(valueObject[field.name]);
+            docValues.push({ value: valueObject[field.name], field: field.name });
         }
         // save values
-        this.set('values', values);
+        this.set('docValues', docValues);
+    },
+
+    ///////////////////////////////////////////////////////
+    // Actions
+    ///////////////////////////////////////////////////////
+
+    actions: {
+        selectDocument(id) {
+            // get checkbox status
+            const checked = !this.$(`#checkbox-${id}`).prop('checked');
+            this.set('selected', checked);
+        }
     }
 
 });
