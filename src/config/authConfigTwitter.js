fd250ae0abe407c9cb3de9d82230c5925214b272
ErikNovak <novak.erik@gmail.com>
2018-01-12 11:02:30 +0100

    Add authentication config placeholders

---
+++ src/config/authConfigTwitter.js
@@ -0,0 +1,6 @@
+// contact fellow developers for authentication info
+module.exports = {
+    consumerKey: '{{ twitter-consumer-key }}',
+    consumerSecret: '{{ twitter-consumer-secret }}',
+    callbackURL: '/auth/twitter/callback'
+};
