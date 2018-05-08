1fa17f5d29985ccea9f63c8e09f77b92bbfee637
ErikNovak <novak.erik@gmail.com>
2018-05-08 22:34:13 +0200

    Change button colors; fix dataset upload table

--- src/server/website-source/app/components/method-selection-fields-number/component.js
+++ src/server/website-source/app/components/method-selection-fields-number/component.js
@@ -12,7 +12,6 @@ export default Component.extend({
         this._super(...arguments);
         // set the feature parameters
         let fields = this.get('dataset.fields');
-        console.log(fields);
         this.set('parameters.features', fields.filterBy('type', 'float').map(field =>
             ({ field: field.name, included: true })
         ));
